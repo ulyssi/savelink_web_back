0384422a316edb8eb087c07df8015b39ff7e5a82
CADOUR Ulysse <ulysse.cadour@infotel.com>
2020-12-02 21:09:21 +0100

    revert config

--- src/conf/config.js
+++ src/conf/config.js
@@ -8,11 +8,11 @@ m_i_core_url="savelink.io"
 m_i_core_port="8484"
 m_i_host_mysql="127.0.0.1"
 //supprimer la suite pour travailler en dev
-/*m_i_mysql_database="m_g_pass_mysql_database"
+m_i_mysql_database="m_g_pass_mysql_database"
 m_i_mysql_user="m_g_pass_mysql_user"
 m_i_mysql_password="m_g_pass_mysql_password"
 m_i_boites="m_g_boite"
 m_i_pass_node_expose_port="m_g_pass_node_expose_port"
 m_i_core_url="m_g_core_url"
 m_i_core_port="m_g_core_port"
-m_i_host_mysql="m_g_host_mysql"*/
+m_i_host_mysql="m_g_host_mysql"
