a72cccb1708d541a703859ad46ac904746bce79b
CADOUR Ulysse <ulysse.cadour@infotel.com>
2020-12-04 17:46:55 +0100

    config for dev env

--- src/conf/config.js
+++ src/conf/config.js
@@ -6,14 +6,4 @@ m_i_mysql_password="savelink_db_password"
 m_i_core_url="savelink.io"
 m_i_core_port="8484"
 m_i_host_mysql="127.0.0.1"
-m_i_url_core='savelink.io:8383/savelink/reload.'
-//supprimer la suite pour travailler en dev le fichier de conf rec/prod est dans dev
-/*
-m_i_mysql_database="m_g_pass_mysql_database"
-m_i_mysql_user="m_g_pass_mysql_user"
-m_i_mysql_password="m_g_pass_mysql_password"
-m_i_boites="m_g_boite"
-m_i_pass_node_expose_port="m_g_pass_node_expose_port"
-m_i_core_url="m_g_core_url"
-m_i_core_port="m_g_core_port"
-m_i_host_mysql="m_g_host_mysql" */
+m_i_url_core='savelink.io:8484/savelink/reload.'
