fc053a9188b0b7d7e8980a5e71ef50b4b45783e7
CADOUR Ulysse <ulysse.cadour@infotel.com>
2020-12-18 00:04:56 +0100

    new group controller.

--- src/services/controller/GroupController.js
+++ src/services/controller/GroupController.js
@@ -9,7 +9,7 @@ var user = require('../db/users');
 
 
 
-router.post('/getall', function (req, res) {
+router.post('/getgroup_user', function (req, res) {
     let  c=user.checkpassword(req,res);
     if ( c !== 0){
       
@@ -27,5 +27,95 @@ router.post('/getall', function (req, res) {
 
 });
 
+router.post('/creategroup', function (req, res) {
+    let  c=user.checkpassword(req,res);
+    if ( c !== 0){
+      
+        res.status("400");
+       res.send("Invalid details!");
+       return;
+    }
+      group.create_group(req.body, function (err, rows) {
+
+        if (err)  res.status(400).json(err);
+        else {
+            res.json(rows);
+        }
+    });
+
+});
+
+router.post('/updategroup', function (req, res) {
+    let  c=user.checkpassword(req,res);
+    if ( c !== 0){
+      
+        res.status("400");
+       res.send("Invalid details!");
+       return;
+    }
+      group.updategroup(req.body, function (err, rows) {
+
+        if (err)  res.status(400).json(err);
+        else {
+            res.json(rows);
+        }
+    });
+
+});
+
+router.post('/deletegroup', function (req, res) {
+    let  c=user.checkpassword(req,res);
+    if ( c !== 0){
+      
+        res.status("400");
+       res.send("Invalid details!");
+       return;
+    }
+      group.deletegroup(req.body, function (err, rows) {
+
+        if (err)  res.status(400).json(err);
+        else {
+            res.json(rows);
+        }
+    });
+
+});
+
+
+router.post('/removeuserfromgroup', function (req, res) {
+    let  c=user.checkpassword(req,res);
+    if ( c !== 0){
+      
+        res.status("400");
+       res.send("Invalid details!");
+       return;
+    }
+      group.removeuserfromgroup(req.body, function (err, rows) {
+
+        if (err)  res.status(400).json(err);
+        else {
+            res.json(rows);
+        }
+    });
+
+});
+
+router.post('/addusertogroup', function (req, res) {
+    let  c=user.checkpassword(req,res);
+    if ( c !== 0){
+      
+        res.status("400");
+       res.send("Invalid details!");
+       return;
+    }
+      group.addusertogroup(req.body, function (err, rows) {
+
+        if (err)  res.status(400).json(err);
+        else {
+            res.json(rows);
+        }
+    });
+
+});
 
 module.exports = router;
--- src/services/db/group.js
+++ src/services/db/group.js
@@ -4,22 +4,38 @@ require('./mysql/mysql');
 
 var group = {
     getgroup_user: function (group,callback) {
-      qq=connection.query('SELECT group_name from t_group where idt_group in (select idt_group from t_group_user where idt_user =?)',[group.id], callback);
+      qq=connection.query('SELECT * from t_group a INNER JOIN t_group_user b on a.idt_group=b.idt_group  where b.idt_user in (  select  id_user from t_user c  where c.username =? ) ',[group.id], callback);
       return qq;
     },
     create_group: function (group,callback) {
-      qq=connection.query('INSERT INTO group_name,group_visibility VALUES ( ?,?)',[group.name,group.visibility], callback);
+      qq=connection.query('INSERT INTO t_group(group_name,group_visibility) VALUES (?,?)',[group.group,group.visibility], callback);
       return qq;
     },
-    insert_group_user: function (group,callback) {
-      qq=connection.query('SELECT group_name from t_group where idt_group in (select idt_group from t_group_user where idt_user =?)',[group.id], callback);
+ 
+    updategroup: function (group,callback) {
+      qq=connection.query('UPDATE t_group set group_name=? , group_visibility=? where idt_group=? ',[group.group,group.visibility,group.idt_group], callback);
       return qq;
     },
-    delete_group: function (group,callback) {
-      qq=connection.query('SELECT group_name from t_group where idt_group in (select idt_group from t_group_user where idt_user =?)',[group.id], callback);
+
+    deletegroup: function (group,callback) {
+      qq=connection.query(' delete from t_group where idt_group=?  ',[group.idt_group], callback);
       return qq;
-    }
+    },
     
+    removeuserfromgroup: function (group,callback) {
+      qq=connection.query('delete from t_group_user where idt_user in (select id_user from t_user where username=? )',[group.id], callback);
+      return qq;
+    },
+    
+
+    addusertogroup: function (group,callback) {
+      
+  
+         qq=connection.query(' Insert into t_group_user(idt_user, idt_group , priority ) select id_user,idt_group, ? as priority from t_user LEFT JOIN T_group ON TRUE where username=? AND group_name=?  ',[group.priority,group.id,group.group], callback);
+      return qq;
+    },
+
+
 
 
 
